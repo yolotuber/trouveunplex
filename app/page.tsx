fd0092cf639fe6e454af2ffbfdbe8bd6780ef476
yolotuber <joyou2be@gmail.com>
2026-05-21 10:30:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import dynamic from "next/dynamic";
-
-const MapClient = dynamic(
-  () => import("./components/MapClient"),
-  { ssr: false }
-);
+import MapWrapper from "./components/MapWrapper";
 
 export default function Home() {
   return (
     <main>
-      <MapClient />
+      <MapWrapper />
     </main>
   );
 }
