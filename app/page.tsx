7e721c22bb5398603e2853867afa40a9229eed3f
yolotuber <joyou2be@gmail.com>
2026-05-22 20:28:13 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,9 @@
-"use client";
+import dynamic from "next/dynamic";
 
-import { useEffect, useState } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-} from "react-leaflet";
-
-import "leaflet/dist/leaflet.css";
-
-type Propriete = {
-  adresse: string;
-  lat: number;
-  lng: number;
-};
+const MapClient = dynamic(() => import("./MapClient"), {
+  ssr: false,
+});
 
 export default function Home() {
-  const [proprietes, setProprietes] =
-    useState<Propriete[]>([]);
-
-  useEffect(() => {
-    fetch("/api/proprietes")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setProprietes(data);
-      });
-  }, []);
-
-  return (
-    <main style={{ height: "100vh" }}>
-      <MapContainer
-        center={[46.79, -71.38]}
-        zoom={13}
-        style={{
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <TileLayer
-          attribution="&copy; OpenStreetMap"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {proprietes.map((p, i) => (
-          <Marker
-            key={i}
-            position={[p.lat, p.lng]}
-          >
-            <Popup>
-              {p.adresse}
-            </Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </main>
-  );
+  return <MapClient />;
 }
